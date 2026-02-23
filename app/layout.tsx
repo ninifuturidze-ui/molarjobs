813f4e256da0a33e2f63fb9286cdefbd05d1e380
sandro <smuradashvili@gmail.com>
2026-02-23 15:37:31 +0400

    first working prototype

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans, Instrument_Serif } from 'next/font/google'
 import './globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ['latin'],

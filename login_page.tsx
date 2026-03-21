e136443615d2237d02c815a18ec1a40bab0258db
nino puturidze <nino.futuridze20@gruni.edu.ge>
2026-03-21 17:06:10 +0100

    login: add password show/hide toggle

--- login_page.tsx
+++ login_page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
 import Link from 'next/link'
 
 export default function LoginPage() {
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const [loading, setLoading] = useState(false)
   const [tab, setTab] = useState('login')
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setLoading(true)
     await new Promise(r => setTimeout(r, 1200))
